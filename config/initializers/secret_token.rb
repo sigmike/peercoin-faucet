62d58011368c1c5f2d0f395d01626f21979918bb
Michael Witrant <michael@ouvrages-web.fr>
2013-12-29 17:32:21 +0100

    created app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PeercoinFaucet::Application.config.secret_key_base = '0937eaae741ef0a26e6e52b4a11fe11f0bcc5da3f62e7bcb53076ba3cb7d1bf27d379f0c5773e30ce4b316904ff0233dd74c3fd933e11c0f57707fb96ff24bda'
