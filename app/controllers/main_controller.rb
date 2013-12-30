4394cff2b102d910049596bb39e21fa2467083df
Michael Witrant <michael@ouvrages-web.fr>
2013-12-30 23:45:21 +0100

    save coin request and validate address

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,11 @@
 class MainController < ApplicationController
   def index
+    @coin_request = CoinRequest.new
+    @coin_request.attributes = params[:coin_request].permit(:address) if params[:coin_request]
+    if request.post? or request.put?
+      if @coin_request.save
+        redirect_to root_path, flash: {notice: 'Request created'}
+      end
+    end
   end
 end
--- app/models/coin_request.rb
+++ app/models/coin_request.rb
@@ -1,2 +1,9 @@
 class CoinRequest < ActiveRecord::Base
+  validate :validate_address
+
+  def validate_address
+    unless Peercoin.address_valid?(address)
+      errors.add(:address, :invalid)
+    end
+  end
 end
