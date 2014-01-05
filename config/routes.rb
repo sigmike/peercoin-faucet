21475a699a2b1219200a57f5927ca7d8a51a9583
Michael Witrant <michael@ouvrages-web.fr>
2014-01-05 11:31:26 +0100

    disabled on non testnet for now

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ PeercoinFaucet::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  root 'main#index'
-  post '/' => 'main#index'
+  if Peercoin.info["testnet"]
+    root 'main#index'
+    post '/' => 'main#index'
+  else
+    root to: proc { [404, {}, ['Not available yet']] }
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
